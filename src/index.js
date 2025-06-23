0594ac4df2c72803760ef9ef0d9826dd4a56ca02
diawara09 <diawaraholding223@gmail.com>
2025-06-24 00:43:40 +0300

    Add swiper to the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import "./style.css"
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination,Autoplay } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination,Autoplay],
+   
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+   
+     // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+  
+   
+  });
+
